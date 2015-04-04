49cb34dd190f834c5ba0e20796d23f400103f930
abhishek <abhishekpareek29@gmail.com>
2015-04-04 16:18:34 +0530

    errors removed   #9887  syntax errors are removed from mentee_query.php page

--- mentee_query.php
+++ mentee_query.php
@@ -25,32 +25,47 @@ if (isset($_POST['submit'])) {
 $query = $_POST["querytitle"]; 
 $description = $_POST["message"];
 $tags = $_POST["tags"];
-$tagid = 'Select tag_id from tags where tag_keyword="$tags"';
-if (!mysql_query($tagid,$link)) {
-	$in_tag = "INSERT into tags(tag_keyword) values('$tags')";
-	mysql_query($in_tag,$link);
+$tagid = "Select tag_id from tags where tag_keywords='$tags'";
+
+
+// if (!mysql_query($tagid,$link)) {
+// 	$in_tag = "INSERT into tags(tag_keywords) values('$tags')";
+// 	mysql_query($in_tag,$link);
+// }
+$retval = mysql_query( $tagid, $link );
+
+if(! $retval )
+{
+  $intag = 'INSERT into tags(tag_keywords) values("$tags")';
+  $sql = mysql_query( $intag, $link );
+}  
+// if(! $sql )
+// {
+//   	die('Could not insert tag:' . mysql_error());
+// }
+$retval = mysql_query( $tagid, $link );
+if(! $retval)
+{
+  	die('Can not find tag:' . mysql_error());
 }
 
-$import="INSERT into queries(query_title,query_desc,tags,) values('$query','$description','$tagid')";
-mysql_query($import) or die(mysql_error());
 
+$row = mysql_fetch_array($retval, MYSQL_ASSOC);
+echo "<br>{$row['tag_id']}";
+$import="INSERT into queries(query_title,query_desc,tag_id) values('$query','$description',{$row['tag_id']})";
+$jojo = mysql_query($import,$link);
+if(! $jojo )
+{
+ die('Could not insert: ' . mysql_error());
+}
 }
 
-else {
-
-        print "Upload new csv by browsing to file and clicking on Upload<br />\n";
-
-        print "<form enctype='multipart/form-data' action='upload.php' method='post'>";
-
-        print "File name to import:<br />\n";
-
-        print "<input size='50' type='file' name='filename'><br />\n";
-
-        print "<input type='submit' name='submit' value='Upload'></form>";
-
+else 
+{
+        echo "FAILED";
 }
 
-
+mysql_close($link);
 ?>
 
 
